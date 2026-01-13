efe2809e0f2eca52187260afa24d0a9d103c22a4
DSD-resilience <infantry97@gmail.com>
2026-01-13 06:51:35 -0500

    initial commit

---
+++ model.py
@@ -0,0 +1,39 @@
+# model.py
+
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+
+def create_cnn_model(input_shape=(28, 28, 1), num_classes=10):
+    """
+    Creates a simple Convolutional Neural Network (CNN) model.
+
+    Parameters:
+        input_shape (tuple): Shape of the input images (height, width, channels).
+        num_classes (int): Number of output classes.
+
+    Returns:
+        model (tf.keras.Model): Compiled CNN model.
+    """
+    model = models.Sequential()
+
+    # Convolutional Block 1
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    # Convolutional Block 2
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+
+    # Flatten and Dense Layers
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dropout(0.5))  # helps prevent overfitting
+    model.add(layers.Dense(num_classes, activation='softmax'))
+
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
